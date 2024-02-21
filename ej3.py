30486ca0cfd9135c18292a37f0ef8719193ac3c5
BrandonHernandez2708 <brandon.her27@gmail.com>
2024-02-20 19:26:01 -0600

    prueba

--- ej3.py
+++ ej3.py
@@ -8,7 +8,7 @@ def parentesis_balancedos(cadena):
             if not pila or pila.pop() != '(':#verifica que la pila no este vacia y si no esta vacia vrifica que no tenga un parentesis izquierdo
                 return False 
     return not pila
-cadena = "(3,4,55,4,3,3)"
+cadena = "(3,4,55,4,3,3"
 parentesis_balancedos("(3,4,55,4,3,3)")
 verificacion=parentesis_balancedos(cadena)
 print(verificacion)             
